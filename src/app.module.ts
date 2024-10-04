f42b5d5252e4689e349448281a89821e4643c766
Edielson-Rodrigues <edielsonrodrigues567@gmail.com>
2024-10-03 23:03:09 -0300

    feat: database connection init

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,7 +1,15 @@
 import { Module } from '@nestjs/common';
+import { ConfigModule } from '@nestjs/config';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { TypeORMConfig } from './config/typeorm.config';
 
 @Module({
-  imports: [],
+  imports: [
+    ConfigModule.forRoot({
+      isGlobal: true,
+    }),
+    TypeOrmModule.forRootAsync(TypeORMConfig)
+  ],
   controllers: [],
   providers: [],
 })
