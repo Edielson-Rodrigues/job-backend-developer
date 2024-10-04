54ce2d4f65197026a6f76c1cc6f21ddb489dec17
Edielson-Rodrigues <edielsonrodrigues567@gmail.com>
2024-10-03 22:59:39 -0300

    chore: create connection with typeorm

---
+++ src/config/typeorm.config.ts
@@ -0,0 +1,18 @@
+import { TypeOrmModuleAsyncOptions } from '@nestjs/typeorm';
+
+export const TypeORMConfig: TypeOrmModuleAsyncOptions = {
+  useFactory: () => ({
+    type: 'mysql',
+    host: process.env.DB_HOST,
+    port: parseInt(process.env.DB_PORT),
+    database: process.env.DB_NAME,
+    username: process.env.DB_USERNAME,
+    password: process.env.DB_PASSWORD,
+    entities: ['dist/**/*.entity{.ts,.js}'],
+    migrations: ['dist/migrations/*{.ts,.js}'],
+    synchronize: true,
+    migrationsRun: true,
+    migrationsTableName: 'migrations_TypeORM',
+    migrationsTransactionMode: 'all'
+  })
+};
